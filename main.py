7f4fd88cfdf915be013aa09645bed995e3900bb9
Miftahul Jannat <jannat27120@gmail.com>
2026-09-16 00:24:11 +0600

    Add Pydantic model for to-do creation with validation

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
 from fastapi import FastAPI, Depends, HTTPException
 from sqlalchemy.orm import Session
+from pydantic import BaseModel, Field
 import models
 from models import Todos
 from typing import Annotated
 from database import engine, SessionLocal
+from fastapi.responses import JSONResponse
 
 app = FastAPI()
+
+# Pydantic models for request validation
+class Todo(BaseModel):
+    id : int
+    title : str
+    description : str = Field(max_length=100)
+    priority : int = Field(gt=0, lt=6)
+    completed : bool
 
 models.Base.metadata.create_all(bind=engine)
 
@@ -31,3 +41,12 @@ def read_specific_todos(db : db_dependency, todo_id : int):
         return specific_todo
     else:
         raise HTTPException(status_code=404, detail='To do not found')
+
+# Create a new to-do
+@app.post('/create/')
+def create_todos(db : db_dependency, new_todo : Todo):
+    todo_model = Todos(**new_todo.model_dump())
+    db.add(todo_model)
+    db.commit()
+    
+    return JSONResponse(status_code=201, content={'message' : 'To do created successfully'})
