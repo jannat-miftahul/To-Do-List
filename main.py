4fca1e606e0de2099ffbf50c3b7884b9acee8cfa
Miftahul Jannat <jannat27120@gmail.com>
2026-09-15 23:45:42 +0600

    Read all to-dos

--- main.py
+++ main.py
@@ -1,8 +1,24 @@
-from fastapi import FastAPI, Depends, HTTPException
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm import Session
 import models
 from models import Todos
-from database import engine
+from typing import Annotated
+from database import engine, SessionLocal
 
 app = FastAPI()
 
 models.Base.metadata.create_all(bind=engine)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+db_dependency = Annotated[Session, Depends(get_db)]
+
+# Read all to-dos
+@app.get('/')
+def read_todos(db : db_dependency):
+    return db.query(Todos).all()
