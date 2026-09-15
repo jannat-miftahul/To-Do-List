347b4d0b947bda3a438b1d621bef34b81398594f
Miftahul Jannat <jannat27120@gmail.com>
2026-09-16 00:11:12 +0600

    Add endpoint to read specific to-do by ID with error handling

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, HTTPException
 from sqlalchemy.orm import Session
 import models
 from models import Todos
@@ -22,3 +22,12 @@ db_dependency = Annotated[Session, Depends(get_db)]
 @app.get('/')
 def read_todos(db : db_dependency):
     return db.query(Todos).all()
+
+# Read specific to-do by ID
+@app.get('/todo/{todo_id}')
+def read_specific_todos(db : db_dependency, todo_id : int):
+    specific_todo = db.query(Todos).filter(Todos.id == todo_id).first()
+    if specific_todo is not None:
+        return specific_todo
+    else:
+        raise HTTPException(status_code=404, detail='To do not found')
