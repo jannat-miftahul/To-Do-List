2190b519a970b52f44f22a7a7d450659b34f46de
Miftahul Jannat <jannat27120@gmail.com>
2026-09-24 01:47:11 +0600

    Add user creation

--- router/auth.py
+++ router/auth.py
@@ -1,7 +1,30 @@
 from fastapi import FastAPI, APIRouter
+from pydantic import BaseModel
+from models import Users
+from fastapi.responses import JSONResponse
 
 router = APIRouter()
 
-@router.get('/auth')
-def authentication():
-    return {'user' : 'authenticated'}
+class CreateUser(BaseModel):
+    email: str
+    username: str
+    first_name: str
+    last_name: str
+    hashed_password: str
+    role: str = 'user'  # Default role is 'user'
+
+@router.post('/create-user/')
+def create_user(db : db_dependency, new_user : CreateUser):
+    user_model = Users(
+        email=new_user.email,
+        username=new_user.username,
+        first_name=new_user.first_name,
+        last_name=new_user.last_name,
+        hashed_password=new_user.hashed_password,
+        role=new_user.role
+    )
+
+    db.add(user_model)
+    db.commit()
+    
+    return JSONResponse(status_code=201, content={'message' : 'User created successfully'})
