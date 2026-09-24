c36262a988e7be5b80c28b41a1f782097feb9040
Miftahul Jannat <jannat27120@gmail.com>
2026-09-25 01:34:37 +0600

    Enhance user creation with password hashing

--- router/auth.py
+++ router/auth.py
@@ -1,18 +1,34 @@
-from fastapi import FastAPI, APIRouter
+from fastapi import FastAPI, APIRouter, Depends
 from pydantic import BaseModel
 from models import Users
 from fastapi.responses import JSONResponse
+from sqlalchemy.orm import Session
+from passlib.context import CryptContext
+from typing import Annotated
+from database import SessionLocal
 
 router = APIRouter()
+
+bcrypt_context = CryptContext(schema=['bcrypt'], deprecated='auto')
 
 class CreateUser(BaseModel):
     email: str
     username: str
     first_name: str
     last_name: str
-    hashed_password: str
+    password: str
     role: str = 'user'  # Default role is 'user'
 
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+db_dependency = Annotated[Session, Depends(get_db)]
+
+# Create a new user
 @router.post('/create-user/')
 def create_user(db : db_dependency, new_user : CreateUser):
     user_model = Users(
@@ -20,7 +36,7 @@ def create_user(db : db_dependency, new_user : CreateUser):
         username=new_user.username,
         first_name=new_user.first_name,
         last_name=new_user.last_name,
-        hashed_password=new_user.hashed_password,
+        hashed_password=bcrypt_context.hash(new_user.password),
         role=new_user.role
     )
 
